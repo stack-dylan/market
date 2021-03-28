f57fc95d1fe7eccc61af771f14752b7799e36a3b
Dylan M Ang <dylanmouang@gmail.com>
2021-03-27 21:22:59 -0700

    Fixed Market add

--- Market.cpp
+++ Market.cpp
@@ -4,15 +4,19 @@
 Market::Market(void) {}
 
 void Market::print(void) const {
-	std::cout << "Market::print() called" << std::endl;
+	std::cout << "Name Price Stock" << std::endl;
+	std::cout << "================" << std::endl;
 	for (auto i : hold) {
 		i.first->print();
-		std::cout << i.second << std::endl;
+		std::cout << " " << i.second << std::endl;
 	}
 }
 
 void Market::add(Item* i, int count) {
-	if (hold.count(i)) {
+	if (!hold.count(i)) {
 		hold[i] = count;
+	}
+	else {
+		hold[i] += count;
 	}
 }
